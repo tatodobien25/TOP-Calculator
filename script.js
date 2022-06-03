8dd02755a697f1283c8261dcea4f612a18bf387c
Sergio Correa Guerrero <s.sergiocorreag@gmail.com>
2022-06-02 20:22:26 -0500

    add more styles. add functionality for basic operations with 2 operands. clear key and equals key are working

--- script.js
+++ script.js
@@ -27,3 +27,53 @@ function operate(operator, num1, num2) {
         return 'Operator is not valid';
     }
 }
+
+function numberClickHandler(e) {
+    if (operator === '') {
+        firstOperand += this.textContent;
+        resultsDisplay.textContent = firstOperand;
+    } else {
+        secondOperand += this.textContent;
+        historyDisplay.textContent = firstOperand + operator;
+        resultsDisplay.textContent = secondOperand;
+    }
+}
+
+function operatorClickHandler(e) {
+    // console.log(this.textContent);
+    if (operator === '') {
+        operator = this.textContent;
+    } else return;
+}
+
+function equalsClickHandler(e) {
+    if (operator === '') {
+        return;
+    } else {
+        historyDisplay.textContent = firstOperand + operator + secondOperand;
+        resultsDisplay.textContent = operate(operator, +firstOperand, +secondOperand).toString();
+    }
+}
+function clearClickHandler(e) {
+    operator = '';
+    firstOperand = '';
+    secondOperand = '';
+    historyDisplay.textContent = '';
+    resultsDisplay.textContent = '0';
+}
+
+const numberKeys = Array.from(document.querySelectorAll('.num-btn'));
+const historyDisplay = document.getElementById('history-input');
+const resultsDisplay = document.getElementById('result');
+const operatorKeys = [document.getElementById('minus-btn'), document.getElementById('plus-btn'),
+document.getElementById('divide-by-btn'), document.getElementById('multiply-by-btn')];
+const equalsKey = document.getElementById('equals-btn');
+const clearKey = document.getElementById('clear-btn');
+let firstOperand = '';
+let secondOperand = '';
+let operator = '';
+
+numberKeys.forEach(key => key.addEventListener('click', numberClickHandler));
+operatorKeys.forEach(key => key.addEventListener('click', operatorClickHandler));
+equalsKey.addEventListener('click', equalsClickHandler);
+clearKey.addEventListener('click', clearClickHandler);
